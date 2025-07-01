302f750cfd9931fbfe4226fd8958f83048b94948
Hafidz Zaenul Ahkam <hafidz.zaenul.tif23@polban.ac.id>
2025-07-01 21:02:47 +0700

    edit edit

--- server.js
+++ server.js
@@ -10,6 +10,23 @@ app.use(express.json());
 app.use("/api/auth", require("./routes/auth.routes"));
 app.use("/api/posts", require("./routes/post.routes"));
 
+app.get("/", (req, res) => {
+  res.send(`
+    <h2>Blog Case API</h2>
+    <p>Berikut daftar endpoint yang tersedia:</p>
+    <ul>
+      <li><b>POST</b> /api/auth/register – Register user</li>
+      <li><b>POST</b> /api/auth/login – Login user</li>
+      <li><b>GET</b> /api/posts – Lihat semua post</li>
+      <li><b>GET</b> /api/posts/:id – Lihat post berdasarkan ID</li>
+      <li><b>POST</b> /api/posts – Buat post (login required)</li>
+      <li><b>PUT</b> /api/posts/:id – Update post (login & author only)</li>
+      <li><b>DELETE</b> /api/posts/:id – Hapus post (login & author only)</li>
+    </ul>
+    <p>Gunakan Insomnia/Postman untuk mengakses endpoint ini.</p>
+  `);
+});
+
 if (process.env.NODE_ENV !== "test") {
     // Database sync
     db.sequelize.sync().then(() => {
